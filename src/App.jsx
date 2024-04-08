b8751bddbaacb5d6aec6fb253f584c6159e7e5a7
TomChuck <m.tomczyk@gymmaster.io>
2024-04-09 01:08:26 +0200

    Added roll's count and game time counter

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import { useEffect, useState } from "react";
 import Die from "./Die";
 import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
+import Timer from "./Timer";
+
 const App = () => {
 	const [dice, setDice] = useState(allNewDice());
 	const [tenzies, setTenzies] = useState(false);
+	const [numberOfRolls, setNumberOfRolls] = useState(0);
+	const [isRunning, setIsRunning] = useState(false);
+	const [time, setTime] = useState(0);
 
 	useEffect(() => {
 		const allHeld = dice.every(die => die.isHeld);
@@ -12,6 +17,7 @@ const App = () => {
 		const allSameValue = dice.every(die => die.value === firstValue);
 		if (allHeld && allSameValue) {
 			setTenzies(true);
+			setIsRunning(false);
 		}
 	}, [dice]);
 
@@ -36,13 +42,17 @@ const App = () => {
 			setDice(oldDice =>
 				oldDice.map(die => (die.isHeld ? die : generateNewDie()))
 			);
+			setNumberOfRolls(numberOfRolls + 1);
 		} else {
 			setTenzies(false);
 			setDice(allNewDice());
+			setNumberOfRolls(0);
+			setTime(0);
 		}
 	}
 
 	const holdDice = id => {
+		setIsRunning(true);
 		setDice(oldDice =>
 			oldDice.map(die => {
 				return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
@@ -68,6 +78,8 @@ const App = () => {
 				current value between rolls.
 			</p>
 			<div className='dice-container'>{diceElements}</div>
+			<h3 className='roll-count'>Roll's count: {numberOfRolls}</h3>
+			<Timer isRunning={isRunning} time={time} setTime={setTime} />
 			<button className='roll-dice' onClick={rollDice}>
 				{tenzies ? "New Game" : "Roll"}
 			</button>
